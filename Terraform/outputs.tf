7ab78ad47a080859e1de9a0299b8d7b14a363712
Nagham_Mohamed <nagham_mohamed@localhost.localdomain>
2025-06-22 19:58:09 +0300

    fix(main.tf & outputs.tf): Add ecr module in main.tf and output its name & url.

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -38,10 +38,10 @@ output "alb_dns_name" {
 
 output "aws_ecr_repository_url" {
   description = "The URL of the ECR repository"
-  value       = aws_ecr_repository.ecr_repo.repository_url
+  value       = module.ecr.aws_ecr_repository_url
 }
 
 output "aws_ecr_repository_name" {
   description = "Name of the ECR repo"
-  value = aws_ecr_repository.ecr_repo.name
+  value = module.ecr.aws_ecr_repository_name
 }
